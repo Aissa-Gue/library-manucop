82ddaa9ee8ae5cf841bf413300266b5dae48dcd4
Aissa-Gue <AissaStarDz@gmail.com>
2021-03-30 11:12:32 +0100

    change login style

--- login.php
+++ login.php
@@ -11,44 +11,149 @@ include 'config.php';
     <link rel="stylesheet" href="bootstrap-4.5.3/css/bootstrap-rtl.min.css">
     <link rel="stylesheet" href="bootstrap-4.5.3/bootstrap-icons-1.2.1/font/bootstrap-icons.css">
 
-    <!-- FONTS -->
-    <link href="https://fonts.googleapis.com/css2?family=Cairo&display=swap" rel="stylesheet">
-
     <!-- my styles -->
     <link rel="stylesheet" href="css/style.css">
 
+    <!-- FONTS -->
+    <link rel="stylesheet" href="css/fonts.css">
+
     <title><?php echo $ProjTitle ?></title>
 </head>
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
 
-<body>
-    <div class="container-fluid background_img_login">
-        <header class="text-center mb-4">
-            <img class="img-fluid my_header_img" src="img/biblio_header.gif" alt="header">
-        </header>
-        <div class="row justify-content-md-center">
-            <div class="col-md-3 p-3 my_radious my_form_bg">
-                <form action="check.php" method="post" enctype="multipart/form-data">
-                    <div class="mb-3">
-                        <h4 class="text-center mb-5">
-                            <strong>تسجيل الدخول</strong>
-                        </h4>
-                        <label for="username" class="form-label">اسم المستخدم</label>
-                        <input type="text" name="username" class="form-control" id="username"
-                            placeholder="أدخل اسم المستخدم">
-                    </div>
-                    <div class="mb-4">
-                        <label for="password" class="form-label">كلمة المرور</label>
-                        <input type="password" name="password" class="form-control" id="password"
-                            placeholder="أدخل كلمة المرور">
-                    </div>
-                    <div class="mb-4 text-center mt-5">
-                        <input type="submit" name="login" value="تسجيل الدخول" class="btn btn-danger">
-                    </div>
+body a {
+    color: darkorchid;
+    text-decoration: none;
+}
+
+.login-form-wrap {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 1200px;
+    height: 600px;
+    margin: 0 auto;
+    overflow: hidden;
+    border-radius: 10px;
+    box-shadow: 0px 0px 47px 0px rgba(0, 0, 0, 0.47);
+    margin-top: 20px;
+    background: #fff;
+
+    background-image: url(img/login_form.jpg);
+    background-size: cover;
+    background-position: 570px 0px;
+    object-fit: cover;
+}
+
+.welcome-text p {
+    font-size: 13px;
+}
+
+.left-section {
+    width: 450px;
+    height: 540px;
+    float: left;
+}
+
+.right-section {
+    width: 750px;
+    position: relative;
+    height: 100vh;
+    background-image: url(img/login.png);
+    background-size: cover;
+    /* background-position: 750px 0px; */
+    object-fit: cover;
+}
+
+.welcome-text {
+    position: absolute;
+    max-width: 750px;
+    text-align: center;
+    top: 25%;
+    color: #9F7833;
+    transform: translateY(-50%);
+}
+
+.title {
+    text-align: center;
+    margin-top: 80px;
+    color: #292929;
+    line-height: 30px;
+    margin-bottom: 30px;
+}
+
+.form {
+    max-width: 260px;
+    margin: 0 auto;
+    text-align: center;
+}
+
+.form input[type=text],
+.form input[type=password] {
+    width: 300px;
+    padding: 14px 12px;
+    border-radius: 30px;
+    border: 1px solid #ddd;
+    margin-bottom: 15px;
+    outline: none;
+    box-shadow: 0px 0px 42px #d4d4d4;
+}
+
+.form input[type="submit"] {
+    display: inline-block;
+    border: none;
+    width: 170px;
+    padding: 11px;
+    border-radius: 30px;
+    cursor: pointer;
+    letter-spacing: 2px;
+    margin: 20px 0px;
+    transition: 0.3s all ease-in-out;
+}
+
+.form input[type="submit"]:focus {
+    outline: none;
+}
+
+.form input[type="submit"]:hover {
+    background: #111;
+}
+
+.create-account {
+    font-weight: 500;
+    font-size: 14px;
+    color: #424242;
+    margin-top: 10px;
+}
+</style>
+
+<body class="">
+    <div class="login-form-wrap">
+
+        <div class="right-section">
+            <div class="welcome-text">
+                <!-- <h1>برنامج نساخ المخطوطات</h1> -->
+            </div>
+        </div>
+
+        <div class="left-section">
+            <div class="title mb-5">
+                <h1>مرحبا !</h1>
+            </div>
+            <div class="form">
+                <form class="" action="check.php" method="post">
+                    <input type="text" name="username" placeholder="أدخل اسم المستخدم">
+                    <input type="password" name="password" placeholder="أدخل كلمة المرور">
+                    <input type="submit" name="login" class="btn btn-info" value="تسجيل الدخول">
                 </form>
             </div>
         </div>
     </div>
-
     <script src="bootstrap-4.5.3/js/bootstrap.bundle.min.js"></script>
     <script src="bootstrap-4.5.3/jquery-3.3.1.js"></script>
 </body>
